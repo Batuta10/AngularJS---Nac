0be86f273871b12f1ccd937c971e1ed3f5df1ce4
Batuta10 <batuta2604@gmail.com>
2017-08-22 08:32:10 -0300

    Generete Componetes and Routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {Routes} from '@angular/router'
+import {HomeComponent} from './home/home.component'
+import {HeaderComponent} from './header/header.component'
+import {FooterComponent} from './footer/footer.component'
+import {AlunosComponent} from './alunos/alunos.component'
+import {ContatosComponent} from './contatos/contatos.component'
+import {DisciplinasComponent} from './disciplinas/disciplinas.component'
+
+export const ROUTES:Routes = [
+    {path:'/', component: HomeComponent},
+    {path:'/home', component:HomeComponent},
+    {path:'/disciplinas', component:DisciplinasComponent},
+    {path:'/alunos', component:AlunosComponent},
+    {path:'/contatos', component:ContatosComponent}
+]
